351498f6e18bc8af1fe424a4dc8e165d1dc77324
rf-opssupport <rf-opssupport@kansocloud.com>
2023-12-19 19:40:20 +0530

    Added entrypoint.sh file

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add config/creds copying here..
+aws s3 cp s3://duploservices-dev-configs-new-167414264568/document-processor/GMU_admin.json /app/assets
+
+cd /app
+# Add any other scripts here...
+# Start the service
+# npm run start
+pm2-runtime src/index.js
