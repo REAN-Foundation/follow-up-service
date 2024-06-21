b7bda117f8d097a8b450450bce20a5b532d45d88
nisha-inflectionzone <112902619+nisha-inflectionzone@users.noreply.github.com>
2024-06-21 17:19:19 +0530

    reverted sslcertificate change in db service mongoclient code (#25)

--- app/services/common_service/db_service.py
+++ app/services/common_service/db_service.py
@@ -14,10 +14,7 @@ class DatabaseService(IStorageService):
 
     async def connect_storage(self,collect_prefix):
         connection_url = os.getenv("MONGODB_URL")
-        ssl_cert = os.getenv("SSL_CERT_REQS")
-        allow_invalid = os.getenv("ALLOW_INVALID_CERT")
-        client = MongoClient(connection_url, ssl_cert_reqs = ssl_cert, tlsAllowInvalidCertificates = allow_invalid)
-        # db = client['document_db']
+        client = MongoClient(connection_url)
         db = client.get_database(os.getenv("DATABASE_NAME"))
         if(collect_prefix == 'gghn'):
             record = db.get_collection(os.getenv("GGHN_COLLECTION_NAME"))
